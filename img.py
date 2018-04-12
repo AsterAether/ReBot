d6af3e56fadbbc14d3fcfb20207038c05c2e73e1
AsterAether <tommi99.tn@gmail.com>
2018-04-12 17:09:29 +0100

    cropping workarounds

--- img.py
+++ img.py
@@ -28,36 +28,39 @@ def image_perception_hash(filename):
 
 
 def image_crop(filename):
-    filename = 'files/' + filename
+    try:
+        filename = 'files/' + filename
 
-    # Load the image in black and white (0 - b/w, 1 - color).
-    img = cv2.imread(filename, 0)
+        # Load the image in black and white (0 - b/w, 1 - color).
+        img = cv2.imread(filename, 0)
 
-    # Get the height and width of the image.
-    h, w = img.shape[:2]
+        # Get the height and width of the image.
+        h, w = img.shape[:2]
 
-    # Invert the image to be white on black for compatibility with findContours function.
-    imgray = 255 - img
-    # Binarize the image and call it thresh.
-    ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
+        # Invert the image to be white on black for compatibility with findContours function.
+        imgray = 255 - img
+        # Binarize the image and call it thresh.
+        ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
 
-    # Find all the contours in thresh. In your case the 3 and the additional strike
-    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    # Calculate bounding rectangles for each contour.
-    rects = [cv2.boundingRect(cnt) for cnt in contours]
+        # Find all the contours in thresh. In your case the 3 and the additional strike
+        _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        # Calculate bounding rectangles for each contour.
+        rects = [cv2.boundingRect(cnt) for cnt in contours]
 
-    # Calculate the combined bounding rectangle points.
-    top_x = min([x for (x, y, w, h) in rects])
-    top_y = min([y for (x, y, w, h) in rects])
-    bottom_x = max([x + w for (x, y, w, h) in rects])
-    bottom_y = max([y + h for (x, y, w, h) in rects])
+        # Calculate the combined bounding rectangle points.
+        top_x = min([x for (x, y, w, h) in rects])
+        top_y = min([y for (x, y, w, h) in rects])
+        bottom_x = max([x + w for (x, y, w, h) in rects])
+        bottom_y = max([y + h for (x, y, w, h) in rects])
 
-    # Draw the rectangle on the image
-    # out = cv2.rectangle(img, (top_x, top_y), (bottom_x, bottom_y), (0, 255, 0), 2)
-    img = cv2.imread(filename)
-    img = img[top_y:bottom_y, top_x:bottom_x]
-    # Save it as out.jpg
-    cv2.imwrite(filename, img)
+        # Draw the rectangle on the image
+        # out = cv2.rectangle(img, (top_x, top_y), (bottom_x, bottom_y), (0, 255, 0), 2)
+        img = cv2.imread(filename)
+        img = img[top_y:bottom_y, top_x:bottom_x]
+        # Save it as out.jpg
+        cv2.imwrite(filename, img)
+    except:
+        print(filename + ' ERROR CROPPING')
 
 
 def compare_image_ssim(filename1, filename2):
